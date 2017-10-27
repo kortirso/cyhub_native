1e2e915723991637f5f9f11618a087ccdf72ff2c
kortirso <kortirso@gmail.com>
2017-10-27 13:38:37 +0300

    modify login form

--- components/Login.js
+++ components/Login.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, StyleSheet, KeyboardAvoidingView } from 'react-native';
+import { View, StyleSheet, KeyboardAvoidingView, Image } from 'react-native';
 import LoginForm from './LoginForm';
 
 export default class Login extends Component {
@@ -11,6 +11,7 @@ export default class Login extends Component {
     return (
       <KeyboardAvoidingView behavior='padding' style={styles.container}>
         <View style={styles.loginContainer}>
+          <Image source={require('../assets/images/cyhub-icon-full.png')} style={styles.welcomeImage} />
           <LoginForm login={this._login.bind(this)} fontLoaded={this.props.fontLoaded} />
         </View>
       </KeyboardAvoidingView>
@@ -20,5 +21,6 @@ export default class Login extends Component {
 
 const styles = StyleSheet.create({
   container: {flex: 1, backgroundColor: '#F2F2F2'},
+  welcomeImage: {width: 250, resizeMode: 'contain'},
   loginContainer: {alignItems: 'center', flexGrow: 1, justifyContent: 'center'}
 });
